db4917b06795e3deb0e1a60a3a52f1de20974ec8
Hồ Tá Quý <taquyit@gmail.com>
2017-10-03 22:45:07 +0700

    Admin page and template

---
+++ EnglishForKid/EnglishForKid/Areas/Admin/Controllers/GiaoVienController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishForKid.Areas.Admin.Controllers
+{
+    public class GiaoVienController : Controller
+    {
+        // GET: GiaoVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ThongTinGiaoVien()
+        {
+            return View();
+        }
+        public ActionResult AddGV()
+        {
+            return View();
+        }
+    }
+}
