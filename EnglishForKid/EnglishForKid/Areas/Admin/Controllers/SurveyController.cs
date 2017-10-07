20e3ad709f104ce2ef884138b95913214d79b0ad
hiennguyen311 <hiennguyenthi979@gmail.com>
2017-10-07 13:52:27 +0700

    UI Admin
    Hien + Dat up giao diện

---
+++ EnglishForKid/EnglishForKid/Areas/Admin/Controllers/SurveyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishForKid.Areas.Admin.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Admin/Survey
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Xem()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
