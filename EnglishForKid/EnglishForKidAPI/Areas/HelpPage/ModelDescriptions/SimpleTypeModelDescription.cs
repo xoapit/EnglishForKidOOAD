9ffd6ef27226a42fa4db0cdfd65f90111e69cff0
Hồ Tá Quý <taquyit@gmail.com>
2017-10-08 20:20:52 +0700

    Fix ckeditor
    fix conflic of last commit and change version ckeditor from old version 2012 to lastest version. Remove some folders that is unnecessary

---
+++ EnglishForKid/EnglishForKidAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EnglishForKidAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ EnglishForKid/Trangchu/Trangchu/TrangchuAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace EnglishForKid.Areas.Trangchu
+{
+    public class TrangchuAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Trangchu";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Trangchu_default",
+                "Trangchu/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
