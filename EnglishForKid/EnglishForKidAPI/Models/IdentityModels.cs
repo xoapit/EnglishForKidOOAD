4c32a0ea77e248114646a464362987a8b4ca3130
Hồ Tá Quý <taquyit@gmail.com>
2017-10-09 19:46:55 +0700

    init identity

--- EnglishForKid/EnglishForKidAPI/Models/IdentityModels.cs
+++ EnglishForKid/EnglishForKidAPI/Models/IdentityModels.cs
@@ -9,6 +9,7 @@ namespace EnglishForKidAPI.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -21,7 +22,7 @@ namespace EnglishForKidAPI.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EnglishForKids", throwIfV1Schema: false)
         {
         }
         
