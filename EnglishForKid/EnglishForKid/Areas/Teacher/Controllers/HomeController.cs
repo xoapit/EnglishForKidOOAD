bcfe6f59f695b736128b40a3ad21bb1c4b9ded1d
Hồ Tá Quý <taquyit@gmail.com>
2017-10-10 14:12:16 +0700

    teacher area

---
+++ EnglishForKid/EnglishForKid/Areas/Teacher/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishForKid.Areas.Teacher.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Teacher/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Teacher/Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Teacher/Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teacher/Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Teacher/Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Teacher/Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EnglishForKid/EnglishForKid/Areas/Teacher/Controllers/LessonController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishForKid.Areas.Teacher.Controllers
+{
+    public class LessonController : Controller
+    {
+        // GET: Teacher/Lesson
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Teacher/Lesson/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Teacher/Lesson/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teacher/Lesson/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Lesson/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Teacher/Lesson/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Lesson/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Teacher/Lesson/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
