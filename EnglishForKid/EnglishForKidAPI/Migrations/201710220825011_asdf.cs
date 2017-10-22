aec9c865efee58ad1816962def270feb29d716f8
Đinh Hữu Quân <huuquan95@gmail.com>
2017-10-22 17:01:07 +0700

    Revert "Revert "something""
    This reverts commit a9a8dfc804f075abe7ae6364630bc4fb2adbf230.

---
+++ EnglishForKid/EnglishForKidAPI/Migrations/201710220825011_asdf.cs
@@ -0,0 +1,16 @@
+namespace EnglishForKidAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class asdf : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
