74e75c6eb853d0f9d965988af87644fcdf67c7ea
Tin-QT <32033672+Tin-QT@users.noreply.github.com>
2017-10-26 23:44:35 +0700

    Fix lesson api can not get data

--- EnglishForKid/EnglishForKid/Service/LessonDataStore.cs
+++ EnglishForKid/EnglishForKid/Service/LessonDataStore.cs
@@ -9,7 +9,7 @@ using EnglishForKid.Models.ViewModels;
 
 namespace EnglishForKid.Service
 {
-    public class LessonDataStore : BaseDataStore, IDataStore<Lesson>
+    public class LessonDataStore : BaseDataStore
     {
         public async Task<bool> AddItemAsync(Lesson item)
         {
@@ -28,7 +28,7 @@ namespace EnglishForKid.Service
             return await Task.FromResult(response.IsSuccessStatusCode);
         }
 
-        public async Task<Lesson> GetItemAsync(Guid id)
+        public async Task<Lesson> GetItemAsync(String id)
         {
             Lesson lesson = null;
 
@@ -72,9 +72,12 @@ namespace EnglishForKid.Service
             throw new NotImplementedException();
         }
 
-        public Task<bool> UpdateItemAsync(Lesson item)
+        public async Task<bool> UpdateItemAsync(Lesson item)
         {
-            throw new NotImplementedException();
+            string path = "/api/Lessons/" + item.ID;
+            HttpResponseMessage response = await client.PutAsJsonAsync(path, item).ConfigureAwait(false);
+            return await Task.FromResult(response.IsSuccessStatusCode); 
+
         }
 
     }
